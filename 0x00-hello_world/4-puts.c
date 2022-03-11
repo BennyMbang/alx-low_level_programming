3b4e1dbd86036d7338ad37cbcbdab1780524b413
BennyMbang <99411187+BennyMbang@users.noreply.github.com>
2022-03-11 06:49:47 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
     * Return: Always 0 (Success)
     */
 
-int main(void)
+int main(void) /* let main be entry point*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
