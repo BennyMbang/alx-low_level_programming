a3d3aac607798b075257ac0435a818464c41ed57
BennyMbang <mbenny511@gmail.com>
2022-04-05 16:08:01 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 /**
 * create_array - creates an array of chars* @size: size of the array
 * @c: storaged char
-*
+* @size: size of the array.
 * Return: pointer of an array of chars
 */
 char *create_array(unsigned int size, char c)
